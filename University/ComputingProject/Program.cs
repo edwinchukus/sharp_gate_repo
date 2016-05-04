56e56b57c2cc0eb107f1aba722c1880071a64599
Edwin Nwofor <edwin.nwofor@yahoo.com>
2016-05-04 21:38:24 +0100

    Initial addition of all of our Projects

---
+++ University/ComputingProject/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ComputingProject
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            GoToMeeting();
+        }
+
+        public static void GoToMeeting()
+        {
+            Console.WriteLine("Stop talking in the class");
+            Console.WriteLine("Stop talking in the class");
+        }
+
+    }
+}
---
+++ University/PhysicsProject/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+
+namespace PhysicsProject
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your Reference Temperature");
+            string roomTemp = Console.ReadLine();
+            LoopThroughMethod(roomTemp);
+        }
+
+        private static void LoopThroughMethod(string roomTemp)
+        {
+            int roomTemperature = int.Parse(roomTemp);
+            Console.WriteLine();
+            for (int i = 0; i < roomTemperature; i++)
+            {
+                if (i == 5)
+                {
+                    Console.WriteLine("This AC is now switched off bc temp is " + i);
+                    continue;
+                }
+                WriteMessage();
+            }
+        }
+
+        private static void WriteMessage()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+
+
+
+}
