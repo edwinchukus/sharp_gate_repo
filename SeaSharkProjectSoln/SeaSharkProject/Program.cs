e35740e8be8cef8d71c7ee2aa7e0a54e5aaff609
Edwin Nwofor <edwin.nwofor@yahoo.com>
2016-05-11 22:19:03 +0100

    Treated Constructor properly

--- SeaSharkProjectSoln/SeaSharkProject/Program.cs
+++ SeaSharkProjectSoln/SeaSharkProject/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using SeaSharkProject.calf;
+using SeaSharkProject.facebook;
 
 namespace SeaSharkProject
 {
@@ -10,8 +11,15 @@ namespace SeaSharkProject
     {
         public static void Main(string[] args)
         {
-            
 
+            Elephant elephant = new Elephant("Edwin", "Loveth", "Anney", "Hiltler");
+
+            string name = elephant.GetFirstStudentName();
+            Console.WriteLine(name);
+
+            Elephant pigon = new Elephant("Avan Rolf", "Jeff Morgan", "Roy Fielding", "David Cameron");
+            string name1 = pigon.GetFirstStudentName();
+            Console.WriteLine(name1);
         }
     }
 }
