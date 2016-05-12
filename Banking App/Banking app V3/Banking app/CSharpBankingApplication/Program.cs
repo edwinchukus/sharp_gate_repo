5aa8cef0948170ee784039b002a9366f102ba9f5
Sam Quartey <saq17ay@msn.com>
2016-05-12 22:42:39 +0100

    code changes

--- Banking App/Banking app V3/Banking app/CSharpBankingApplication/Program.cs
+++ Banking App/Banking app V3/Banking app/CSharpBankingApplication/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CSharpBankingApplication.BankLogin;
+using CSharpBankingApplication.Register;
 
 
 namespace CSharpBankingApplication
