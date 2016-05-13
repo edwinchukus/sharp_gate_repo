db8d5fa7ca8e8550a82eda4df18bcc3b2a02c031
Edwin Nwofor <edwin.nwofor@yahoo.com>
2016-05-13 22:56:23 +0100

    Added Code after merge

---
+++ SeaSharkProjectSoln/SeaSharkProject/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SeaSharkProject.basics;
+using SeaSharkProject.calf;
+using SeaSharkProject.facebook;
+
+namespace SeaSharkProject
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //NCrunch 
+            Friday friday = new Friday(5321);
+            
+            friday.GetNewStudentDate("I am a boy, from Nigeria, and I am a grown up man now, I can make a decision.");
+
+
+            friday.GetAndre();
+        }
+
+
+        
+    }
+}
