bd53613b06a6c549c4f05dc8eb43b8d21cd7dbd2
Edwin Nwofor <edwin.nwofor@yahoo.com>
2016-05-20 23:00:10 +0100

    20-05-2016 - Codes from class

--- SeaSharkProjectSoln/SeaSharkProject/Program.cs
+++ SeaSharkProjectSoln/SeaSharkProject/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using SeaLionProject.fish;
 using SeaSharkProject.access_modifiers;
 using SeaSharkProject.basics;
 using SeaSharkProject.calf;
 using SeaSharkProject.facebook;
+using SeaSharkProject.gettersAndSetters;
 
 namespace SeaSharkProject
 {
@@ -11,10 +13,32 @@ namespace SeaSharkProject
     {
         public static void Main(string[] args)
         {
+            Eagle eagle = new Eagle();
 
-         
-            
-  
+            //            Console.WriteLine(eagle.eagleName);
+            //
+            //            eagle.eagleName = "Black Eagle";
+            //            Console.WriteLine(eagle.eagleName);
+
+            string name = eagle.GetEagleName();
+            Console.WriteLine(name);
+            eagle.SetHasWings(false);
+
+
+            eagle.SetEagleName("Black Eagle");
+
+
+            string name1 = eagle.GetEagleName();
+            Console.WriteLine(name1);
+
+            eagle.Fly();
+
+            // Object Oriented Programming Langauge 
+            // Encapsulation
+            // Inheritance
+            // Polymorphism
+
+
 
         }
 
---
+++ SeaSharkProjectSoln/SeaSharkProject/gettersAndSetters/Eagle.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SeaSharkProject.gettersAndSetters
+{
+    public class Eagle
+    {
+
+
+        private string eagleName = "White Eagle";
+        private double eagleWieght = 5.00;
+        private bool hasWings = true;
+        private int numberOfEaglesEyes = 2;
+        private string name;
+
+        public string GetEagleName()
+        {
+            return eagleName;
+        }
+
+        private double GetEagleWeight()
+        {
+            return eagleWieght;
+        }
+
+        
+        public bool GetHasWings()
+        {
+            return hasWings;
+        }
+
+
+        public int GetNumberOfEyes()
+        {
+            return numberOfEaglesEyes;
+        }
+
+       
+        public void SetEagleName(string eagleName)
+        {
+            this.eagleName = eagleName;
+        }
+
+        public void SetEagleWeight(double eagleWieght)
+        {
+            this.eagleWieght = eagleWieght;
+        }
+
+        public void SetHasWings(bool hasWings)
+        {
+            this.hasWings = hasWings;
+        }
+
+        public void SetEagleName(int numberOfEaglesEyes)
+        {
+            this.numberOfEaglesEyes = numberOfEaglesEyes;
+        }
+
+
+
+        public void Fly()
+        {
+            Console.WriteLine("Yes I am an eagle, the king of birds");
+        }
+
+
+    }
+}
