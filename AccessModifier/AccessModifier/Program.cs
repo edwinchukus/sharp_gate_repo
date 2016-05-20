b3d537b4b69ab6168cee02b0fb8419e9aea54395
casianus Shaadzeka <casi@lounge-pc>
2016-05-20 20:36:46 +0100

    Added Access modifiers created by Casi

---
+++ AccessModifier/AccessModifier/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PublicClass publicClass =new PublicClass();
+            publicClass.PublicTest();
+
+            ProtectedClass protectedClass = new ProtectedClass();
+            protectedClass.Test2();
+        }
+    }
+}
