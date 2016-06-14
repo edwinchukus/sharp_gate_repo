da7aa9d1d45ae423779a1c14d7a809ca865b3f90
casianus Shaadzeka <casi@lounge-pc>
2016-06-14 21:20:54 +0100

    interface

---
+++ TUESDAY/TUESDAY/interfaceNameSpace/implimentation/toyotaCamry.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TUESDAY.interfaceNameSpace.implimentation
+{
+   public class toyotaCamry:Icar
+    {
+       public void Move()
+       {
+           throw new NotImplementedException();
+       }
+
+       public string Brake()
+       {
+           throw new NotImplementedException();
+       }
+
+       public double speed()
+       {
+           throw new NotImplementedException();
+       }
+
+       public bool isTheCarMoving()
+       {
+           throw new NotImplementedException();
+       }
+    }
+}
