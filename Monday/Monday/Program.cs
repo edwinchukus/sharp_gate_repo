627e3e7190365f59afb60a8109721831ae5380a5
casianus Shaadzeka <casi@lounge-pc>
2016-06-16 21:47:36 +0100

    updates on abstract

---
+++ Monday/Monday/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Monday.abstractNameSapec;
+
+namespace Monday
+{
+    class Program
+    {
+       public static void Main(string[] args)
+        {
+         Dog dog= new Dog ("Whisky", 5, true);
+           string name = dog.ShowAnimalName();
+           dog.Run();
+
+        }
+
+    }
+}
