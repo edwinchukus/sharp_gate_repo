1958dc04b40bc19c61f12952f789c80b2c0cde6a
casianus Shaadzeka <casi@lounge-pc>
2016-06-17 11:21:28 +0100

    Error exception handling updated

---
+++ shopping/shopping/Exceltion Handling/Calculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shopping.Exceltion_Handling
+{
+   public class Calculator
+    {
+
+
+       public double DivideNumber(int a, int b )
+       {
+           double an = a/b;
+           return an;
+       }
+    }
+}
